11af23acd6475ded182f264f763b87b2699a39dd
elasri21 <islame.edu@gmail.com>
2024-01-19 07:30:52 -0800

    ts 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,7 +6,7 @@ package {'python3-pip':
   ensure => installed,
 }
 exec { 'install_flash':
-  command => '/usr/bin/pip3 install flash=2.1.0',
+  command => '/usr/bin/pip3 install flash==2.1.0',
   path    => ['/usr/bin'],
   require => Package['python3-pip'],
 }
