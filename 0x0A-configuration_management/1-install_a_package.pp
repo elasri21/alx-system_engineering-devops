17d022492f8630d587a6cfc6962f4283e2af3f13
elasri21 <islame.edu@gmail.com>
2024-01-19 08:50:02 -0800

    ts 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #!/usr/bin/pup
 # installs flask using puppet
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
