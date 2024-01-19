d4ceca08f3c8a49c7bda8150e7f2355072f0841e
elasri21 <islame.edu@gmail.com>
2024-01-19 08:06:27 -0800

    ts 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -9,7 +9,7 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => '/usr/bin/pip3 install flask -v 2.1.0',
   path    => ['/usr/bin'],
   require => Package['python3-pip'],
 }
