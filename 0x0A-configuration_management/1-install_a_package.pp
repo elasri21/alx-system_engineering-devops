c48a14b442b6f16868f432648dc6bba39954cacc
elasri21 <islame.edu@gmail.com>
2024-01-19 08:21:33 -0800

    ts 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
 #!/usr/bin/pup
 # installs flask using puppet
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+class { 'python':
+  version => 'system',
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'build-essential':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Package['python3-pip', 'build-essential'],
 }
