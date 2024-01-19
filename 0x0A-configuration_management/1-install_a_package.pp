decb0c206a47ab2e9c60ca31d0b8672818477acf
elasri21 <islame.edu@gmail.com>
2024-01-19 07:26:50 -0800

    ts 00

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
 # installs flash using puppet
+class {'python':
+  version => 'system',
+}
+package {'python3-pip':
+  ensure => installed,
+}
 exec { 'install_flash':
   command => '/usr/bin/pip3 install flash=2.1.0',
   path    => ['/usr/bin'],
