d2b82a2b638f6ea4cecc1c6e60021078883065b5
elasri21 <islame.edu@gmail.com>
2024-01-19 07:37:05 -0800

    ts 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
 # installs flask using puppet
+class {'python':
+  version => 'sestem',
+}
+package { 'python3-pip':
+  ensure => installed,
+}
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
