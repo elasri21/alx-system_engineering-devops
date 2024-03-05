842fbd82809bf4f9493809d5f99d6e28f5c26f96
elasri21 <islame.edu@gmail.com>
2024-03-05 07:20:44 -0800

    ts

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns
+the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers
+    Args:
+        subreddit: subreddit argument"""
+    info = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if info.status_code >= 300:
+        return 0
+    return info.json().get("data").get("subscribers")
