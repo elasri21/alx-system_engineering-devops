1ae124d996fe2990e76af9544340e1518e657cec
elasri21 <islame.edu@gmail.com>
2024-03-05 13:10:48 -0800

    ts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,7 @@
 the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
