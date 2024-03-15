1061cac111bfba142a78226940bce9efdb998bdd
elasri21 <islame.edu@gmail.com>
2024-03-15 04:41:32 -0700

    ts

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
 # Add and enable the user Holberton to login and open files
-exec { 'incease-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/',
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
+# Increase soft file limit for Holberton user.
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
